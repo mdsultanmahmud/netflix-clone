187e14facb6cd594b4b86b295c65e0bca4aed36e
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2023-01-14 22:02:16 +0600

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import MainLayout from "./layout/MainLayout"
 import Home from "./pages/Home/Home"
-
+import Login from "./sharedPages/Login/Login"
+import Register from "./sharedPages/Register/Register"
+import './style.scss'
 function App() {
   const router = createBrowserRouter([
     {
@@ -11,14 +13,21 @@ function App() {
         {
           path:'/',
           element: <Home></Home>
+        },
+        {
+          path:'/login',
+          element: <Login></Login>
+        },
+        {
+          path:'/register',
+          element:<Register></Register>
         }
       ]
     }
   ])
   return (
-    <div>
-      <h1>netflix clone </h1>
-    </div>
+    <RouterProvider router={router}>
+    </RouterProvider>
   )
 }
 
