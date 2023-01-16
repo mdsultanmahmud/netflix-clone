0ed92227d67d8b4709f5c8204ed68cd885f6719d
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2023-01-16 19:10:48 +0600

    register page done

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyD1iaFiaQGmxFSONoA7-T5vqdWYQCNNZPc",
+  authDomain: "netflix-clone-react-c0fb6.firebaseapp.com",
+  projectId: "netflix-clone-react-c0fb6",
+  storageBucket: "netflix-clone-react-c0fb6.appspot.com",
+  messagingSenderId: "894075366599",
+  appId: "1:894075366599:web:01b427a7092a0f8d51a2e2"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app 
--- src/sharedPages/Register/Register.jsx
+++ src/sharedPages/Register/Register.jsx
@@ -1,25 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BackgroundImage from '../BackgroundImage/BackgroundImage';
 import Header from '../Header/Header';
-
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged} from 'firebase/auth'
+import app from '../../firebase/firebase.init';
+import { useNavigate } from 'react-router-dom';
+const auth = getAuth(app)
 const Register = () => {
+    const [showInput, setShwoInput] = useState(true)
+    const [signUpValue, setSignUPValue] =  useState({
+        email: '',
+        password:''
+    })
+    const navigate = useNavigate()
+    const handleRegister = async(event) =>{
+        event.preventDefault()
+        try{
+            const {email, password} = signUpValue
+            console.log(email, password)
+            await createUserWithEmailAndPassword(auth, email, password)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+    useEffect(() =>{
+        onAuthStateChanged(auth, (user) =>{
+            if(user){
+                navigate('/')
+            }
+        })
+    } ,[])
     return (
         <div className='register-container'>
             <BackgroundImage></BackgroundImage>
             <div className="register-sub-container">
                 <Header></Header>
                 <div className="register-content">
-                    <h1>Unlimited Movies, TV showes, and more.</h1>
-                    <h2>Watch anywhere. Cancel anytime</h2>
-                    <p>Ready to watch? Enter your email to create or restart your membership.</p>
-                    <form>
-                        <div>
-                            <input type="email" placeholder='email address' />
-                            <input type="password" placeholder='enter password' />
-                            <button>Get Started</button>
-                        </div>
-                        <button>sign up</button>
-                    </form>
+                    <div>
+                        <h1>Unlimited Movies, TV showes, and more.</h1>
+                        <h2>Watch anywhere. Cancel anytime</h2>
+                        <p className='text'>Ready to watch? Enter your email to create or restart your membership.</p>
+                        <form>
+                            <div className={`${!showInput ? 'hidden' : 'form-container'}`}>
+                                <input 
+                                name='email'
+                                type="email" 
+                                placeholder='email address' 
+                                value={signUpValue.email} 
+                                onChange={(e) => setSignUPValue({
+                                    ...signUpValue,
+                                    [e.target.name]: e.target.value
+                                })}
+                                />
+                                {
+                                    !showInput ?
+                                    <input 
+                                    name='password' 
+                                    type="password" 
+                                    placeholder='enter password' 
+                                    value={signUpValue.password}
+                                    onChange={(e) => setSignUPValue({
+                                        ...signUpValue,
+                                        [e.target.name]: e.target.value
+                                    })}
+                                    />
+                                    :
+                                    <button onClick={() => setShwoInput(false)}>Get Started</button>
+                                }
+                            </div>
+                            <button onClick={handleRegister} style={{marginTop: '10px'}}>sign up</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
